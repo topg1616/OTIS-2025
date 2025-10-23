787604a56b148764ef6394642938fcd6d2746712
Лыщенко Кирилл <kvimiki@gmail.com>
2025-10-23 22:34:09 +0300

    changed model.cpp

--- trunk/ii002713/task_02/src/model.cpp
+++ trunk/ii002713/task_02/src/model.cpp
@@ -23,10 +23,20 @@ double Nonlinear(const NonlinearParams& p, double u, double uPrev, double y, dou
     return p.a * y - p.b * (yPrev * yPrev) + p.c * u + p.d * std::sin(uPrev);
 }
 
-// Симуляция нелинейной модели на заданное количество шагов.
-// p      — параметры модели (a, b, c, d)
-// u      — входной сигнал
-// steps  — число шагов симуляции
+/*
+ Симуляция нелинейной модели.
+ Функция выполняет пошаговую симуляцию нелинейной системы.
+
+ param p      Структура NonlinearParams, содержащая параметры модели (a, b, c, d).
+ param u      Входной сигнал на каждом шаге симуляции.
+ param steps  Количество шагов симуляции.
+
+ Поведение:
+  - Инициализирует текущие и предыдущие состояния y и u.
+  - На каждом шаге вычисляет новое значение y с помощью функции Nonlinear().
+  - Выводит текущее значение y на каждом шаге.
+  - Обновляет предыдущие значения yPrev и uPrev для следующего шага.
+ */
 void simulateNonlinear(const NonlinearParams& p, double u, int steps) {
     std::cout << "Nonlinear model" << '\n';
     double y = 0.0;
