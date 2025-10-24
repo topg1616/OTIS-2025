b31aad05dd478092a3f25dc90958e4621f9ed7a2
Лыщенко Кирилл <kvimiki@gmail.com>
2025-10-24 23:41:54 +0300

    changed model.cpp

--- trunk/ii002713/task_02/src/model.cpp
+++ trunk/ii002713/task_02/src/model.cpp
@@ -4,37 +4,17 @@
 
 /**
  * Линейная модель.
- *
- * Вычисляет выход y линейной системы по формуле:
  * y_next = a * y + b * u
- *
- * @param a  Параметр модели.
- * @param b  Параметр модели.
- * @param u  Входной сигнал.
- * @param y  Предыдущее значение выхода.
- * @return   Новое значение выхода y.
  */
 double Linear(double a, double b, double u, double y) {
     return a * y + b * u;
 }
 
 /**
- * Симуляция линейной модели.
- *
- * Функция выполняет пошаговую симуляцию линейной системы.
- *
- * @param a      Параметр модели.
- * @param b      Параметр модели.
- * @param u      Входной сигнал на каждом шаге симуляции.
- * @param steps  Количество шагов симуляции.
- *
- * Поведение:
- * - Инициализирует текущее значение y нулём.
- * - На каждом шаге вычисляет новое значение y с помощью функции Linear().
- * - Выводит текущее значение y на каждом шаге.
+ * Симуляция линейной модели
  */
 void simulateLinear(double a, double b, double u, int steps) {
-    std::cout << "Linear model" << '\n';
+    std::cout << "Linear Model Simulation:" << '\n';
     double y = 0.0;
     for (int i = 0; i < steps; ++i) {
         std::cout << "Step " << i << ": " << y << '\n';
@@ -45,35 +25,14 @@ void simulateLinear(double a, double b, double u, int steps) {
 
 /**
  * Нелинейная модель.
- *
- * Вычисляет выход y нелинейной системы по формуле:
  * y_next = a * y - b * yPrev^2 + c * u + d * sin(uPrev)
- *
- * @param p      Структура NonlinearParams, содержащая параметры модели (a, b, c, d).
- * @param u      Текущий входной сигнал.
- * @param uPrev  Предыдущий входной сигнал.
- * @param y      Текущее значение выхода.
- * @param yPrev  Предыдущее значение выхода.
- * @return       Новое значение выхода y.
  */
 double Nonlinear(const NonlinearParams& p, double u, double uPrev, double y, double yPrev) {
     return p.a * y - p.b * (yPrev * yPrev) + p.c * u + p.d * std::sin(uPrev);
 }
 
 /**
- * Симуляция нелинейной модели.
- *
- * Функция выполняет пошаговую симуляцию нелинейной системы.
- *
- * @param p      Структура NonlinearParams, содержащая параметры модели (a, b, c, d).
- * @param u      Входной сигнал на каждом шаге симуляции.
- * @param steps  Количество шагов симуляции.
- *
- * Поведение:
- * - Инициализирует текущие и предыдущие состояния y и u.
- * - На каждом шаге вычисляет новое значение y с помощью функции Nonlinear().
- * - Выводит текущее значение y на каждом шаге.
- * - Обновляет предыдущие значения yPrev и uPrev для следующего шага.
+ * Симуляция нелинейной модели
  */
 void simulateNonlinear(const NonlinearParams& p, double u, int steps) {
     std::cout << "Nonlinear Model Simulation:" << '\n';
