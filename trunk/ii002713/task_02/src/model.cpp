0bd9814b8dc5c49b58bc6e782591cfdf53c22645
Лыщенко Кирилл <kvimiki@gmail.com>
2025-10-24 11:16:26 +0300

    changed model.cpp

--- trunk/ii002713/task_02/src/model.cpp
+++ trunk/ii002713/task_02/src/model.cpp
@@ -2,12 +2,37 @@
 #include <iostream>
 #include <cmath>
 
-// линейная модель
+/**
+ * Линейная модель.
+ *
+ * Вычисляет выход y линейной системы по формуле:
+ * y_next = a * y + b * u
+ *
+ * @param a  Параметр модели.
+ * @param b  Параметр модели.
+ * @param u  Входной сигнал.
+ * @param y  Предыдущее значение выхода.
+ * @return   Новое значение выхода y.
+ */
 double Linear(double a, double b, double u, double y) {
     return a * y + b * u;
 }
 
-// симуляция линейной модели
+/**
+ * Симуляция линейной модели.
+ *
+ * Функция выполняет пошаговую симуляцию линейной системы.
+ *
+ * @param a      Параметр модели.
+ * @param b      Параметр модели.
+ * @param u      Входной сигнал на каждом шаге симуляции.
+ * @param steps  Количество шагов симуляции.
+ *
+ * Поведение:
+ * - Инициализирует текущее значение y нулём.
+ * - На каждом шаге вычисляет новое значение y с помощью функции Linear().
+ * - Выводит текущее значение y на каждом шаге.
+ */
 void simulateLinear(double a, double b, double u, int steps) {
     std::cout << "Linear model" << '\n';
     double y = 0.0;
@@ -18,7 +43,19 @@ void simulateLinear(double a, double b, double u, int steps) {
     std::cout << '\n';
 }
 
-// нелинейная модель
+/**
+ * Нелинейная модель.
+ *
+ * Вычисляет выход y нелинейной системы по формуле:
+ * y_next = a * y - b * yPrev^2 + c * u + d * sin(uPrev)
+ *
+ * @param p      Структура NonlinearParams, содержащая параметры модели (a, b, c, d).
+ * @param u      Текущий входной сигнал.
+ * @param uPrev  Предыдущий входной сигнал.
+ * @param y      Текущее значение выхода.
+ * @param yPrev  Предыдущее значение выхода.
+ * @return       Новое значение выхода y.
+ */
 double Nonlinear(const NonlinearParams& p, double u, double uPrev, double y, double yPrev) {
     return p.a * y - p.b * (yPrev * yPrev) + p.c * u + p.d * std::sin(uPrev);
 }
