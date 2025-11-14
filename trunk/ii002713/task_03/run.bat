27544019bf0cd6cff8e97477edb7fa3b4322843d
Лыщенко Кирилл <kvimiki@gmail.com>
2025-11-14 21:45:59 +0300

    change files

--- trunk/ii002713/task_03/run.bat
+++ trunk/ii002713/task_03/run.bat
@@ -1,8 +1,8 @@
 @echo off
 REM Windows-only build script (run.bat)
 REM Requires Windows, Visual Studio and CMake installed.
-REM For Linux/macOS use run.sh instead.
-REM Build Debug, run tests, main program and generate HTML docs
+REM Documentation is now hosted on GitHub Pages:
+REM https://topg1616.github.io/OTIS-2025/
 
 REM Set build directory inside src
 set BUILD_DIR=%~dp0src\build
@@ -10,36 +10,31 @@ set BUILD_DIR=%~dp0src\build
 REM Create build folder if not exists
 if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%"
 
-REM Configure and build Debug
 echo.
-echo Building Debug configuration
+echo   Building Debug configuration (CMake)
+echo.
+
 cmake -S "%~dp0src" -B "%BUILD_DIR%" -A x64 -DCMAKE_BUILD_TYPE=Debug
 cmake --build "%BUILD_DIR%" --config Debug
 
-REM Run Google Test
 echo.
-echo Running Google Tests
+echo          Running Google Tests
+echo.
+
 "%BUILD_DIR%\Debug\runTests_ii002713_task03.exe"
-echo.
-echo Google Tests finished
 
-REM Run main program
 echo.
-echo Running main program (pid_sim)
+echo        Running main program (pid_sim)
+echo.
+
 "%BUILD_DIR%\Debug\pid_sim.exe"
-echo.
-echo Main program finished
-
-REM Generate Doxygen HTML documentation
-echo.
-echo Generating Doxygen HTML documentation
-cmake --build "%BUILD_DIR%" --target doc
-
-REM Open HTML documentation in default browser
-if exist "%~dp0doc\html\index.html" (
-    start "" "%~dp0doc\html\index.html"
-)
 
 echo.
-echo All done: Tests, main program, documentation
+echo    Opening GitHub Pages documentation
+echo.
+
+start "" "https://topg1616.github.io/OTIS-2025/"
+
+echo.
+echo       All done: Build, Tests, Docs
 pause
